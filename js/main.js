b9cffd2a386765a88f486d6c4e3fdfa62b9bc2ae
Shevchenko Andrey <demorest49de@gmail.com>
2023-01-05 11:49:05 +0300

    c

--- js/main.js
+++ js/main.js
@@ -5,33 +5,47 @@ const getInput = () => {
   const result = prompt(`Введите строку`);
   return result ? result.trim().toLowerCase() : null;
 };
+
+const del = () => {
+  if (list.lastElementChild) {
+    list.removeChild(list.lastElementChild);
+    console.log(list.lastElementChild);
+  }
+};
+
+const clear = () => {
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+};
+
+const doDefault = (userInput) => {
+  const li = document.createElement('li');
+  li.textContent = userInput;
+  li.classList.add('item');
+  list.append(li);
+};
+
 const addLi = (userInput) => {
   console.log(': ', userInput);
   switch (true) {
     case userInput === 'del': {
-      if (list.lastElementChild) {
-        list.removeChild(list.lastElementChild);
-        console.log(list.lastElementChild);
-      }
-      return addLi(getInput());
+      del();
     }
     case userInput === 'clear': {
-      while (list.firstChild) {
-        list.removeChild(list.firstChild);
-      }
-      return addLi(getInput());
+      clear();
     }
     case userInput === null || userInput === '' || userInput === 'exit': {
-      return;
+      return false;
     }
     default: {
-      const li = document.createElement('li');
-      li.textContent = userInput;
-      li.classList.add('item');
-      list.append(li);
-      return addLi(getInput());
+      doDefault(userInput);
     }
   }
+  return true;
 };
 
-addLi(getInput());
+
+do {
+} while (addLi(getInput()));
+
